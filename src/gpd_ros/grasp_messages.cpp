7c7c2008fb66a5e8b2a6d3c3e95f2950f59be2ca
flynnbm <flynn.m.brian@gmail.com>
2025-09-19 12:32:02 -0400

    update functions returning a Vector3 (toMsg() --> toMsg2()), Vector3 overload for toMsg() wasn't working as expected

--- src/gpd_ros/grasp_messages.cpp
+++ src/gpd_ros/grasp_messages.cpp
@@ -1,4 +1,5 @@
 #include <gpd_ros/grasp_messages.h>
+#include <geometry_msgs/msg/vector3.hpp>
 #include <tf2_eigen/tf2_eigen.hpp>
 
 gpd_ros::msg::GraspConfigList GraspMessages::createGraspListMsg(const std::vector<std::unique_ptr<gpd::candidate::Hand>>& hands, 
@@ -20,9 +21,9 @@ gpd_ros::msg::GraspConfig GraspMessages::convertToGraspMsg(const gpd::candidate:
   gpd_ros::msg::GraspConfig msg;
 
   msg.position = tf2::toMsg(hand.getPosition());
-  msg.approach = tf2::toMsg(hand.getApproach());
-  msg.binormal = tf2::toMsg(hand.getBinormal());
-  msg.axis = tf2::toMsg(hand.getAxis());
+  msg.approach = tf2::toMsg2(hand.getApproach());
+  msg.binormal = tf2::toMsg2(hand.getBinormal());
+  msg.axis = tf2::toMsg2(hand.getAxis());
   msg.width.data = hand.getGraspWidth();
   msg.score.data = hand.getScore();
   msg.sample = tf2::toMsg(hand.getSample());
